20a4fea51f0d6c149b3ebf76461d35883d08558d
SwetaTiwari31 <115275106+SwetaTiwari31@users.noreply.github.com>
2022-10-07 21:46:35 +0530

    Update evenorodd_asgn.py

--- evenorodd_asgn.py
+++ evenorodd_asgn.py
@@ -1,6 +1,7 @@
 Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> a =9
-... if a%2 == 0:
-...     print(a,' is even')
-... else:
+a = 9
+if a%2 == 0:
+    print(a,' is even')
+else:
+    print(a,' is odd')
